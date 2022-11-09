91254af4fe53445655364623c4b00abd8ae6c46a
Collin Thornton <collin.thornton@swri.org>
2022-11-09 17:07:24 -0600

    began wrapping reach class and interfaces. trouble with shared_ptrs

--- reach_core/scripts/run_reach_study.py
+++ reach_core/scripts/run_reach_study.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
 
-from reach_core.reach_core_python import YAMLNode, Path, runReachStudy
+import reach_core.reach_core_python as rp
 import argparse
+import os
+import shutil
+
+os.environ["REACH_PLUGINS"] = "libreach_core_plugins"
 
 parser = argparse.ArgumentParser(prog="RunReachStudy", description="Wraps the reach study library in Python")
 parser.add_argument("results_dir", type=str, help="Where to store the results")
 parser.add_argument("config_file", type=str, help="YAML config file")
 parser.add_argument("config_name", type=str, help="Name of this config")
 parser.add_argument('-s', '--sleep', action='store_true', help="Wait 5 seconds before starting")
-
+parser.add_argument('-d', '--delete', action='store_true', help="Delete an existing study at results_dir if it exists")
 args = parser.parse_args()
+
+# Delete the old study if requested
+if args.delete:
+    if os.path.exists(args.results_dir) and os.path.isdir(args.results_dir):
+        print("Deleting the existing study at " + args.results_dir)
+        shutil.rmtree(args.results_dir)
 
 # Delay start up so we can attach a debugger
 if args.sleep == True:
@@ -17,5 +27,44 @@ if args.sleep == True:
     import time
     time.sleep(5)
 
-node = YAMLNode.LoadFile(args.config_file)
-runReachStudy(node, args.config_name, Path(args.results_dir), False)
+class NewIKSolver(rp.IKSolver):
+    def getJointNames(self):
+        return ""
+
+    def solveIK(self, isometry, map):
+        return []
+
+class NewEvaluator(rp.Evaluator):
+    def calculateScore(self):
+        return 0.0
+
+class NewDisplay(rp.Display):
+    def showEnvironment(self):
+        return
+
+    def updateRobotPose(self):
+        return
+
+    def showReachNeighborhood(self):
+        return
+
+    def showResults(self):
+        return
+
+class NewLogger(rp.Logger):
+    def setMaxProgress(self, max_progress):
+        return
+
+    def printProgress(self, progress):
+        return
+
+    def printResults(self, results):
+        return
+
+    def print(self, message):
+        return
+
+study = rp.ReachStudy(NewIKSolver(), NewEvaluator(), rp.TargetPoseGenerator(), NewLogger(), rp.Parameters(), "test")
+
+node = rp.YAMLNode.LoadFile(args.config_file)
+rp.runReachStudy(node, args.config_name, rp.Path(args.results_dir), False)
--- reach_core/src/python/python_bindings.cpp
+++ reach_core/src/python/python_bindings.cpp
@@ -18,13 +18,200 @@
 #include <boost/python.hpp>
 #include <yaml-cpp/yaml.h>
 
+namespace bp = boost::python;
+
 namespace reach
 {
+struct IKSolverWrap : IKSolver, bp::wrapper<IKSolver>
+{
+  std::vector<std::string> getJointNames() const
+  {
+    return this->get_override("getJointNames")();
+  }
+
+  std::vector<std::vector<double>> solveIK(const Eigen::Isometry3d&, const std::map<std::string, double>&) const
+  {
+    return this->get_override("solveIK")();
+  }
+};
+
+struct IKSolverFactoryWrap : IKSolverFactory, bp::wrapper<IKSolverFactory>
+{
+  IKSolver::ConstPtr create(const YAML::Node&) const
+  {
+    return this->get_override("create")();
+  }
+};
+
+struct EvaluatorWrap : Evaluator, bp::wrapper<Evaluator>
+{
+  double calculateScore(const std::map<std::string, double>&) const
+  {
+    return this->get_override("calculateScore")();
+  }
+};
+
+struct EvaluatorFactoryWrap : EvaluatorFactory, bp::wrapper<EvaluatorFactory>
+{
+  Evaluator::ConstPtr create(const YAML::Node&) const
+  {
+    return this->get_override("create")();
+  }
+};
+
+struct TargetPoseGeneratorWrap : TargetPoseGenerator, bp::wrapper<TargetPoseGenerator>
+{
+  VectorIsometry3d generate() const
+  {
+    return this->get_override("generate")();
+  }
+};
+
+struct TargetPoseGeneratorFactoryWrap : TargetPoseGeneratorFactory, bp::wrapper<TargetPoseGeneratorFactory>
+{
+  TargetPoseGenerator::ConstPtr create(const YAML::Node&) const
+  {
+    return this->get_override("create")();
+  }
+};
+
+struct DisplayWrap : Display, bp::wrapper<Display>
+{
+  void showEnvironment() const
+  {
+    this->get_override("showEnvironment")();
+  }
+
+  void updateRobotPose(const std::map<std::string, double>&) const
+  {
+    this->get_override("updateRobotPose")();
+  }
+
+  void showReachNeighborhood(const std::vector<ReachRecord>&) const
+  {
+    this->get_override("showReachNeighborhood")();
+  }
+
+  void showResults(const ReachDatabase&) const
+  {
+    this->get_override("showResults")();
+  }
+};
+
+struct DisplayFactoryWrap : DisplayFactory, bp::wrapper<DisplayFactory>
+{
+  Display::ConstPtr create(const YAML::Node&) const
+  {
+    return this->get_override("create")();
+  }
+};
+
+struct LoggerWrap : Logger, bp::wrapper<Logger>
+{
+  void setMaxProgress(unsigned long)
+  {
+    this->get_override("setMaxProgress")();
+  }
+
+  void printProgress(unsigned long) const
+  {
+    this->get_override("printProgress")();
+  }
+
+  void printResults(const StudyResults&) const
+  {
+    this->get_override("printResults")();
+  }
+
+  void print(const std::string&) const
+  {
+    this->get_override("print")();
+  }
+};
+
+struct LoggerFactoryWrap : LoggerFactory, bp::wrapper<LoggerFactory>
+{
+  Logger::ConstPtr create(const YAML::Node&) const
+  {
+    return this->get_override("create")();
+  }
+};
+
 BOOST_PYTHON_MODULE(reach_core_python)
 {
-  boost::python::class_<YAML::Node>("YAMLNode").def("LoadFile", &YAML::LoadFile);
+  bp::class_<YAML::Node>("YAMLNode").def("LoadFile", &YAML::LoadFile);
+  bp::class_<boost::filesystem::path>("Path", bp::init<std::string>());
 
-  boost::python::class_<boost::filesystem::path>("Path", boost::python::init<std::string>());
-  boost::python::def("runReachStudy", runReachStudy);
+  bp::def("runReachStudy", runReachStudy);
+
+  // Wrap the IKSolvers
+  {
+    bp::class_<IKSolverWrap, boost::noncopyable>("IKSolver")
+        .def("getJointNames", bp::pure_virtual(&IKSolver::getJointNames))
+        .def("solveIK", bp::pure_virtual(&IKSolver::solveIK));
+
+    bp::class_<IKSolverFactoryWrap, boost::noncopyable>("IKSolverFactory")
+        .def("create", bp::pure_virtual(&IKSolverFactory::create))
+        .def("getSection", &IKSolverFactory::getSection);
+  }
+
+  // Wrap the Evaluators
+  {
+    bp::class_<EvaluatorWrap, boost::noncopyable>("Evaluator")
+        .def("calculateScore", bp::pure_virtual(&Evaluator::calculateScore));
+
+    bp::class_<EvaluatorFactoryWrap, boost::noncopyable>("EvaluatorFactory")
+        .def("create", bp::pure_virtual(&EvaluatorFactory::create))
+        .def("getSection", &EvaluatorFactory::getSection);
+  }
+  // Wrap the TargetPoseGenerators
+  {
+    bp::class_<TargetPoseGeneratorWrap, boost::noncopyable>("TargetPoseGenerator")
+        .def("generate", bp::pure_virtual(&TargetPoseGenerator::generate));
+  }
+
+  // Wrap the Displays
+  {
+    bp::class_<DisplayWrap, boost::noncopyable>("Display")
+        .def("showEnvironment", bp::pure_virtual(&Display::showEnvironment))
+        .def("updateRobotPose", bp::pure_virtual(&Display::updateRobotPose))
+        .def("showReachNeighborhood", bp::pure_virtual(&Display::showReachNeighborhood))
+        .def("showResults", bp::pure_virtual(&Display::showResults));
+
+    bp::class_<DisplayFactoryWrap, boost::noncopyable>("DisplayFactory")
+        .def("create", bp::pure_virtual(&DisplayFactory::create))
+        .def("getSection", &DisplayFactory::getSection);
+  }
+
+  // Wrap the Loggers
+  {
+    bp::class_<LoggerWrap, boost::noncopyable>("Logger")
+        .def("setMaxProgress", bp::pure_virtual(&Logger::setMaxProgress))
+        .def("printProgress", bp::pure_virtual(&Logger::printProgress))
+        .def("printResults", bp::pure_virtual(&Logger::printResults))
+        .def("print", bp::pure_virtual(&Logger::print));
+
+    bp::class_<LoggerFactoryWrap, boost::noncopyable>("LoggerFactory")
+        .def("create", bp::pure_virtual(&LoggerFactory::create))
+        .def("getSection", &LoggerFactory::getSection);
+  }
+
+  // Wrap the Parameters
+  {
+    bp::class_<ReachStudy::Parameters>("Parameters")
+        .def_readwrite("max_steps", &ReachStudy::Parameters::max_steps)
+        .def_readwrite("step_improvement_threshold", &ReachStudy::Parameters::step_improvement_threshold)
+        .def_readwrite("radius", &ReachStudy::Parameters::radius);
+  }
+
+  bp::class_<ReachStudy>(
+      "ReachStudy", bp::init<IKSolver::ConstPtr, Evaluator::ConstPtr, TargetPoseGenerator::ConstPtr, Display::ConstPtr,
+                             Logger::ConstPtr, const ReachStudy::Parameters, const std::string&>())
+      .def("load", &ReachStudy::load)
+      .def("save", &ReachStudy::save)
+      .def("getDatabase", &ReachStudy::getDatabase)
+      .def("run", &ReachStudy::run)
+      .def("optimize", &ReachStudy::optimize)
+      .def("getAverageNeighborsCounts", &ReachStudy::getAverageNeighborsCount);
 }
 }  // namespace reach
